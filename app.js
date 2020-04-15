77e066ee666f2b15e9b9c41b02995228322f8dd2
Tobias Lei <tobyglei@gmail.com>
2020-04-15 11:46:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.get("/", (req, res) => res.send("Hello Express / Node.js!"));
+app.get("/", (req, res) => res.send("21云盒子 Hello Express / Node.js 样例!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
